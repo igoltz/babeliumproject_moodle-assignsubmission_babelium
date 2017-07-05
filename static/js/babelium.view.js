e1b61c1e333f293be2ca0c6df7842e8e2048af44
Sergio Anguita <sanguita@irontec.com>
2017-07-05 13:04:32 +0200

    replaced showRecordingMode() function location

--- static/js/babelium.view.js
+++ static/js/babelium.view.js
@@ -90,7 +90,6 @@ function showLoading(value){
 }
 
 function translate(){
-    showRecordingMode(false);
     setButtonsText();
     setTitle();
     setlogs();
@@ -158,39 +157,3 @@ function setCounterColor(color){
         counter.style.color = color;
     }
 }
-
- function showRecordingMode(isRecording){
-    //get recording logo
-    var image = document.getElementsByClassName('recording-image')[0];
-    //show or hide recording image
-    image.style.display = isRecording ? "inherit" : "none";
-    //set recording color
-    var color = isRecording ? "red" : "black";
-    setStatusColor(color);
-    setCounterColor(color);
-    if(isRecording){
-        //update text
-        setStatus(getString('recording_status'));
-        //start counter
-        startClockCountingOn('clock');
-        //update log
-        cstm_log(
-            getString('recording_log')
-        );
-    }
-    else{
-        //update text
-        setStatus(getString('submission_recording_controls'));
-        //stop counter
-        stopClockCountingOn();
-        //start counter
-        if(audio_recorded){
-            cstm_log(
-                getString('recording_stopped_log')
-            );
-            cstm_log(
-                getString('recording_link_log')
-            );
-        }
-    }
- }
